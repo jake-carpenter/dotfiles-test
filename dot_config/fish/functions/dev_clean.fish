0109e18e9ae4a419b10f0621c57ecbea9e3c326c
Jake Carpenter <jake@jakecarpenter.dev>
2025-04-27 20:23:23 -0600

    Add cleanup function for dev artifacts

---
+++ dot_config/fish/functions/dev_clean.fish
@@ -0,0 +1,18 @@
+function dev_clean
+  # Dotnet
+  set -a rules 'bin,obj@*.csproj'
+
+  # Expo-releated stuff
+  set -a rules 'dist,release,ios,android@package.json'
+
+  # Node
+  set -a rules 'node-modules'
+
+  if test -z "$argv[1]"
+    set cwd .
+  else
+    set cwd $argv[1]
+  end 
+
+  projclean --cwd $cwd $rules
+end
